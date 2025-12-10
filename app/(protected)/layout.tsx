898627efac336f309cf3e43c0162fa2a638ed090
kassaaaaa <karog5184@himolde.no>
2025-12-10 18:57:59 +0100

    try to fix app route

---
+++ app/(protected)/layout.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+interface ProtectedLayoutProps {
+  children: React.ReactNode;
+}
+
+export default function ProtectedLayout({ children }: ProtectedLayoutProps) {
+  return <>{children}</>;
+}
