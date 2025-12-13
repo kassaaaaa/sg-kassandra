57dc543be5ffd6ac6f213f9c0bdc018a3ac24c2e
kassaaaaa <karog5184@himolde.no>
2025-12-13 11:05:10 +0100

    3-6 fix

--- supabase/functions/manager-settings/index.ts
+++ supabase/functions/manager-settings/index.ts
@@ -1,4 +1,4 @@
-import { createClient } from 'https://esm.sh/@supabase/supabase-js@2.44.2'
+import { createClient } from 'jsr:@supabase/supabase-js@2'
 import { corsHeaders } from '../_shared/cors.ts'
 
 console.log("Hello from manager-settings!")
